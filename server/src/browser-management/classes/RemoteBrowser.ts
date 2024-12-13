c49e70a1eeab9ef569f1e41738bea8882915ec22
amhsirak <karishmashuklaa@gmail.com>
2024-12-13 22:04:35 +0530

    chrome and chromium user agent

--- server/src/browser-management/classes/RemoteBrowser.ts
+++ server/src/browser-management/classes/RemoteBrowser.ts
@@ -220,8 +220,7 @@ export class RemoteBrowser {
                 password: proxyOptions.password ? proxyOptions.password : undefined,
             };
         }
-        const browserUserAgent = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.5481.38 Safari/537.36";
-
+        const browserUserAgent = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.6778.140 Chromium/131.0.6778.140 Safari/537.36";
 
         contextOptions.userAgent = browserUserAgent;
         this.context = await this.browser.newContext(contextOptions);
