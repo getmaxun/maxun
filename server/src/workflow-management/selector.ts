52b767188eedd3ef3c3053a3e50d054fb9b35e44
amhsirak <karishmashuklaa@gmail.com>
2024-12-17 02:48:38 +0530

    feat: !push parentSelector

--- server/src/workflow-management/selector.ts
+++ server/src/workflow-management/selector.ts
@@ -1015,7 +1015,6 @@ export const getChildSelectors = async (page: Page, parentSelector: string): Pro
           const childPath = getSelectorPath(child);
           if (childPath) {
             selectors.push(childPath);  // Add direct child path
-            selectors.push(parentSelector) 
             selectors = selectors.concat(getAllDescendantSelectors(child));  // Recursively process descendants
           }
         }
