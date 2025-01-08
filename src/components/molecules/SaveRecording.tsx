65cdef3c3becaef2bf7b55cf0be1af0ef6de4808
RohitR311 <rohit.rajan031101@gmail.com>
2025-01-08 13:14:41 +0530

    feat: change comment for button

--- src/components/molecules/SaveRecording.tsx
+++ src/components/molecules/SaveRecording.tsx
@@ -77,8 +77,8 @@ export const SaveRecording = ({ fileName }: SaveRecordingProps) => {
 
   return (
     <div>
-<!--       <Button onClick={() => setOpenModal(true)} variant='contained'  sx={{ marginRight: '20px',backgroundColor: '#ff00c3',color: 'white' }} size="small" color="success">
-        Finish -->
+       {/* <Button onClick={() => setOpenModal(true)} variant='contained'  sx={{ marginRight: '20px',backgroundColor: '#ff00c3',color: 'white' }} size="small" color="success">
+        Finish  */}
 
       <Button onClick={() => setOpenModal(true)} variant="outlined" sx={{ marginRight: '20px' }} size="small" color="success">
         {t('right_panel.buttons.finish')}
