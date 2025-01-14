a324220275400c6567996af51fd2361608824dff
amhsirak <karishmashuklaa@gmail.com>
2025-01-14 23:07:07 +0530

    fix: format

--- src/components/dashboard/NotFound.tsx
+++ src/components/dashboard/NotFound.tsx
@@ -5,7 +5,7 @@ export function NotFoundPage() {
     <div style={{ textAlign: 'center' }}>
       <h1>404 - Page Not Found</h1>
       <p>Oops! This page does not exist.</p>
-      <a href="/" style={{ textDecoration: 'none'}}>Take me to the homepage</a>
+      <a href="/" style={{ textDecoration: 'none' }}>Take me to the homepage</a>
     </div>
   );
 }
