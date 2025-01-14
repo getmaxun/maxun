e643718404526e77d15bccce6fec7da58f5d6f5c
amhsirak <karishmashuklaa@gmail.com>
2025-01-14 23:05:04 +0530

    feat: set text decoration to none

--- src/components/dashboard/NotFound.tsx
+++ src/components/dashboard/NotFound.tsx
@@ -5,7 +5,7 @@ export function NotFoundPage() {
     <div style={{ textAlign: 'center', marginTop: '50px' }}>
       <h1>404 - Page Not Found</h1>
       <p>Oops! This page does not exist.</p>
-      <a href="/">Take me to the homepage</a>
+      <a href="/" style={{ textDecoration: 'none'}}>Take me to the homepage</a>
     </div>
   );
 }
