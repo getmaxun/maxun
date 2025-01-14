0b8d4868b5256461d26986f699c4216c2256ebcd
amhsirak <karishmashuklaa@gmail.com>
2025-01-14 23:04:35 +0530

    fix: not found message

--- src/components/dashboard/NotFound.tsx
+++ src/components/dashboard/NotFound.tsx
@@ -4,8 +4,8 @@ export function NotFoundPage() {
   return (
     <div style={{ textAlign: 'center', marginTop: '50px' }}>
       <h1>404 - Page Not Found</h1>
-      <p>Oops! This page does not. exist.</p>
-      <a href="/">Go to Homepage</a>
+      <p>Oops! This page does not exist.</p>
+      <a href="/">Take me to the homepage</a>
     </div>
   );
 }
