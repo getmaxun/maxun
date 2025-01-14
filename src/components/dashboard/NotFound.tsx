4bd161ab885ca6e2f2b45b1085a0bf139c84df76
amhsirak <karishmashuklaa@gmail.com>
2025-01-14 22:51:31 +0530

    feat: not found component

---
+++ src/components/dashboard/NotFound.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export function NotFoundPage() {
+    return (
+      <div style={{ textAlign: 'center', marginTop: '50px' }}>
+        <h1>404 - Page Not Found</h1>
+        <p>Oops! This page does not. exist.</p>
+        <a href="/">Go to Homepage</a>
+      </div>
+    );
+  }
