8e80067b1dae9c22d22299b9e789cf4ebc613bec
Rohit <rohit.rajan031101@gmail.com>
2025-03-12 17:00:25 +0530

    feat: add browser state for generator

--- server/src/workflow-management/classes/Generator.ts
+++ server/src/workflow-management/classes/Generator.ts
@@ -152,7 +152,7 @@ export class WorkflowGenerator {
     });
     socket.on('activeIndex', (data) => this.generatedData.lastIndex = parseInt(data));
     socket.on('decision', async ({ pair, actionType, decision, userId }) => {
-      const id = browserPool.getActiveBrowserId(userId);
+      const id = browserPool.getActiveBrowserId(userId, "recording");
       if (id) {
         // const activeBrowser = browserPool.getRemoteBrowser(id);
         // const currentPage = activeBrowser?.getCurrentPage();
