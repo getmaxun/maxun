7b3dedca252bd2d436aee4648d97458627cfb9c7
amhsirak <karishmashuklaa@gmail.com>
2025-03-23 13:49:19 +0530

    feat: create pg pool

--- server/src/server.ts
+++ server/src/server.ts
@@ -18,6 +18,8 @@ import { fork } from 'child_process';
 import { capture } from "./utils/analytics";
 import swaggerUi from 'swagger-ui-express';
 import swaggerSpec from './swagger/config';
+import connectPgSimple from 'connect-pg-simple';
+import pg from 'pg';
 
 import session from 'express-session';
 
@@ -29,6 +31,15 @@ app.use(cors({
   credentials: true,
 }));
 app.use(express.json());
+
+const { Pool } = pg;
+const pool = new Pool({
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT,
+});
 
 
 app.use(
