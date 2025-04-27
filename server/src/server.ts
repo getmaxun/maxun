15d4fce194d4e42cda957cb1c7e5962904be09c7
Karishma Shukla <carishmashukla28@gmail.com>
2025-04-27 20:12:01 +0530

    feat: prune session interval

--- server/src/server.ts
+++ server/src/server.ts
@@ -45,6 +45,7 @@ app.use(
       pool: pool,
       tableName: 'session',
       createTableIfMissing: true,
+      pruneSessionInterval: 60 * 60,
     }),
     secret: 'mx-session',
     resave: false, // Do not resave the session if it hasn't changed
