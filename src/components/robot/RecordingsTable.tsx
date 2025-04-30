406def412e189e23a4dc7a16fa78f79800d745d4
Amit Chauhan <36507878+AmitChauhan63390@users.noreply.github.com>
2025-05-01 00:15:10 +0530

    Update RecordingsTable.tsx
    Already done in other PR

--- src/components/robot/RecordingsTable.tsx
+++ src/components/robot/RecordingsTable.tsx
@@ -253,7 +253,7 @@ export const RecordingsTable = ({
         const parsedRows = recordings
         .map((recording: any, index: number) => {
           if (recording?.recording_meta) {
-            const parsedDate = parseDateString(recording.recording_meta.updatedAt);
+            const parsedDate = parseDateString(recording.recording_meta.createdAt);
             
             return {
               id: index,
