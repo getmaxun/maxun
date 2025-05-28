d767e89dfeef76b79bb76d38685557c1f186704e
amhsirak <karishmashuklaa@gmail.com>
2025-05-28 23:56:17 +0530

    chore: lint

--- src/components/api/ApiKey.tsx
+++ src/components/api/ApiKey.tsx
@@ -110,7 +110,7 @@ const ApiKeyManager = () => {
     <Container sx={{ alignSelf: 'flex-start' }}>
       <Typography variant="body1" sx={{ marginTop: '10px', marginBottom: '20px' }}>
         Start by creating an API key below. Then,
-        <a href={`${apiUrl}/api-docs/`} target="_blank" rel="noopener noreferrer" style={{ textDecoration: 'none', marginLeft: '5px', marginRight: '5px' }}> 
+        <a href={`${apiUrl}/api-docs/`} target="_blank" rel="noopener noreferrer" style={{ textDecoration: 'none', marginLeft: '5px', marginRight: '5px' }}>
           test your API
         </a>
         or read the <a href={`${apiUrl}/api-docs/`} target="_blank" rel="noopener noreferrer" style={{ textDecoration: 'none' }}>
