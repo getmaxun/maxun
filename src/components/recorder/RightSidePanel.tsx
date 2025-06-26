3d1a38ec624dc9c6a2669d8fcea50d7526eb14d8
amhsirak <karishmashuklaa@gmail.com>
2025-06-26 22:43:42 +0530

    feat: better error message

--- src/components/recorder/RightSidePanel.tsx
+++ src/components/recorder/RightSidePanel.tsx
@@ -32,7 +32,7 @@ const fetchWorkflow = (id: string, callback: (response: WorkflowFile) => void) =
         throw new Error("No workflow found");
       }
     }
-  ).catch((error) => { console.log(error.message) })
+  ).catch((error) => { console.log(`Failed to fetch workflow:`,error.message) })
 };
 
 interface RightSidePanelProps {
