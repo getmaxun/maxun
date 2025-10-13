bdf726dcc5c892ba6fdbceb7e6a70d1176145377
amhsirak <karishmashuklaa@gmail.com>
2025-10-13 17:49:14 +0530

    feat: sync action description box dark mode bg

--- src/components/action/ActionDescriptionBox.tsx
+++ src/components/action/ActionDescriptionBox.tsx
@@ -16,7 +16,7 @@ const CustomBoxContainer = styled.div<CustomBoxContainerProps>`
   min-height: 100px;
   height: auto;
   border-radius: 5px;
-  background-color: ${({ isDarkMode }) => (isDarkMode ? '#313438' : 'white')};
+  background-color: ${({ isDarkMode }) => (isDarkMode ? '#1d1c1cff' : 'white')};
   color: ${({ isDarkMode }) => (isDarkMode ? 'white' : 'black')};
   margin: 80px 13px 25px 13px;
   box-shadow: 0px 4px 10px rgba(0, 0, 0, 0.1);
