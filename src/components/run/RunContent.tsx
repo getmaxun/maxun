ebbadeb16eb9fa6624163027be484f1e04ee6d59
amhsirak <karishmashuklaa@gmail.com>
2025-10-22 13:17:57 +0530

    feat: remove run logs tab

--- src/components/run/RunContent.tsx
+++ src/components/run/RunContent.tsx
@@ -643,19 +643,6 @@ export const RunContent = ({ row, currentLog, interpretationInProgress, logEndRe
                 }
               }}
             />
-            <Tab
-              label={t('run_content.tabs.log')}
-              value='log'
-              sx={{
-                color: (theme) => theme.palette.mode === 'dark' ? '#fff' : '#000',
-                '&:hover': {
-                  color: '#FF00C3'
-                },
-                '&.Mui-selected': {
-                  color: '#FF00C3',
-                }
-              }}
-            />
           </Tabs>
         </Box>
         <TabPanel value='log'>
