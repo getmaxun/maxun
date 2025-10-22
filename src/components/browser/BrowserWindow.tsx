377fabe8c31113f137028b51ae0b4c54fb332db5
Rohit Rajan <rohit.rajan031101@gmail.com>
2025-10-22 14:03:51 +0530

    fix: remove j char

--- src/components/browser/BrowserWindow.tsx
+++ src/components/browser/BrowserWindow.tsx
@@ -2185,7 +2185,7 @@ export const BrowserWindow = () => {
             0% { transform: rotate(0deg); }
             100% { transform: rotate(360deg); }
           }
-        `}</style>j
+        `}</style>
 
           {(getText === true || getList === true) &&
             !showAttributeModal &&
