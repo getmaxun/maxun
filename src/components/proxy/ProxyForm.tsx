ad5455197fe71587b9ce73d71df03de9a8287a8e
amhsirak <karishmashuklaa@gmail.com>
2026-07-25 19:25:06 +0530

    chore: link byop docs

--- src/components/proxy/ProxyForm.tsx
+++ src/components/proxy/ProxyForm.tsx
@@ -196,7 +196,7 @@ const ProxyForm: React.FC = () => {
                                         </span>
                                     }
                                 />
-                                <a href="" target="_blank" rel="noopener noreferrer" style={{ fontSize: '0.875rem', color: '#ff00c3', textDecoration: 'none' }}>
+                                <a href="https://docs.maxun.dev/byop" target="_blank" rel="noopener noreferrer" style={{ fontSize: '0.875rem', color: '#ff00c3', textDecoration: 'none' }}>
                                     Learn how to configure authenticated proxies here.
                                 </a>
                             </Box>
