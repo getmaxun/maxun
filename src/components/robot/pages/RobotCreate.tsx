e30bf77c97f442ab82dfefca857013af1f8e1a37
amhsirak <karishmashuklaa@gmail.com>
2026-09-09 17:04:41 +0530

    feat: smaller logo

--- src/components/robot/pages/RobotCreate.tsx
+++ src/components/robot/pages/RobotCreate.tsx
@@ -713,8 +713,8 @@ const RobotCreate: React.FC = () => {
             <Box display="flex" flexDirection="column" alignItems="center">
               <img
                 src="https://ik.imagekit.io/ys1blv5kv/maxunlogo.png"
-                width={65}
-                height={57}
+                width={55}
+                height={47}
                 style={{
                   borderRadius: '5px',
                   marginBottom: '30px'
